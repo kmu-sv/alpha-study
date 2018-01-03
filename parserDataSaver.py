209687663cf4a42cfb508de07553b36240e1703a
KooMinJun <mjkoo711@gmail.com>
2018-01-03 11:51:16 -0800

    2018-01-03 Minjun : change parser (html.parser -> lxml)

--- parserDataSaver.py
+++ parserDataSaver.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 req = requests.get('https://beomi.github.io/beomi.github.io_old')
 html = req.text
-soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html, 'lxml')
 my_titles = soup.select(
     'h3 > a'
     )
@@ -19,7 +19,7 @@ data = {}
 
 for title in my_titles:
     data[title.text] = title.get('href')
-    print(title.text)
+#    print(title.text)
 
 with open(os.path.join(BASE_DIR, 'result.json'), 'w+') as json_file:
     json.dump(data, json_file)
